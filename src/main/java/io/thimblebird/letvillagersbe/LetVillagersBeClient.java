33720c75de9c2971ad4193e25c3ed8dd2c6c9e1b
thimblebird <thimblebird@creeation.de>
2022-10-23 07:54:28 +0200

    chore: re-adds client class (for now?)

---
+++ src/main/java/io/thimblebird/letvillagersbe/LetVillagersBeClient.java
@@ -0,0 +1,8 @@
+package io.thimblebird.letvillagersbe;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LetVillagersBeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {}
+}
