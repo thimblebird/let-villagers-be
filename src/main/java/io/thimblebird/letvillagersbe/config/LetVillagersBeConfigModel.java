0f465b341b396172f2e13597c75c4d23143c7886
thimblebird <thimblebird@creeation.de>
2022-10-24 23:30:40 +0200

    fix: adds config sync annotations (server overwrites client config)

--- src/main/java/io/thimblebird/letvillagersbe/config/LetVillagersBeConfigModel.java
+++ src/main/java/io/thimblebird/letvillagersbe/config/LetVillagersBeConfigModel.java
@@ -1,5 +1,6 @@
 package io.thimblebird.letvillagersbe.config;
 
+import io.wispforest.owo.config.Option;
 import io.wispforest.owo.config.annotation.*;
 
 @SuppressWarnings("unused")
@@ -7,32 +8,39 @@ import io.wispforest.owo.config.annotation.*;
 @Config(name = "letvillagersbe-config", wrapperName = "LetVillagersBeConfig")
 public class LetVillagersBeConfigModel {
     @SectionHeader("sectionPlayersConfiguration")
-    public boolean playerDamageVillagerAllowed = false;
+        public boolean playerDamageVillagerAllowed = false;
 
     @SectionHeader("sectionVillagersConfiguration")
-    public boolean villagerWakeUpAllowed = false;
-    public boolean villagerPushingAllowed = false;
-    @RangeConstraint(min = 0.0d, max = 1.0d)
-    public double villagerResponseChance = 0.33d;
-    @RangeConstraint(min = 0.0d, max = 1.0d)
-    public double villagerResponseParticlesChance = 0.66d;
+        public boolean villagerWakeUpAllowed = false;
+
+        @Sync(Option.SyncMode.OVERRIDE_CLIENT)
+        public boolean villagerPushingAllowed = false;
+
+        @RangeConstraint(min = 0.0d, max = 1.0d)
+        public double villagerResponseChance = 0.33d;
+
+        @RangeConstraint(min = 0.0d, max = 1.0d)
+        public double villagerResponseParticlesChance = 0.66d;
 
     @SectionHeader("sectionBedsConfiguration")
-    public boolean bedBreakingAllowed = false;
+        @Sync(Option.SyncMode.OVERRIDE_CLIENT)
+        public boolean bedBreakingAllowed = false;
 
     @SectionHeader("sectionMessagesConfiguration")
-    @Nest
-    @Expanded
-    public ShowMessageWhenTryingTo showMessageWhen = new ShowMessageWhenTryingTo();
-    public static class ShowMessageWhenTryingTo {
-        public boolean wakeUpVillager = true;
-        public boolean breakingBad = true;
-    }
+        @Nest
+        @Expanded
+        public ShowMessageWhenTryingTo showMessageWhen = new ShowMessageWhenTryingTo();
+        public static class ShowMessageWhenTryingTo {
+            public boolean wakeUpVillager = true;
+            public boolean breakingBad = true;
+        }
 
     @SectionHeader("sectionExperimentalConfiguration")
-    public boolean bedBreakingUsingPistonsAllowed = false;
+        @Sync(Option.SyncMode.OVERRIDE_CLIENT)
+        public boolean bedBreakingUsingPistonsAllowed = false;
 
     @SectionHeader("sectionNotImplementedConfiguration")
-    public boolean playerProjectileDamageAllowed = false;
-    public boolean highlightVillagerName = true;
+        public boolean playerProjectileDamageAllowed = false;
+
+        public boolean highlightVillagerName = true;
 }
